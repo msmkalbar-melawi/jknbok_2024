18555d524f9cf92ddc6d300481ccfc6900f7419c
andika-saputra2605 <andika6994@gmail.com>
2024-02-29 20:43:17 +0700

    first all

---
+++ application/helpers/custom_helper.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+function jns_ang_jkn($skpd)
+{
+    //  echo 'DIKA';
+    $ci =& get_instance();
+    $data = "SELECT jns_ang FROM jkn_trhrka a WHERE a.kd_skpd='$skpd' AND a.tgl_dpa IN(SELECT MAX(tgl_dpa) FROM jkn_trhrka c WHERE c.kd_skpd=a.kd_skpd AND status='1')";
+    return $ci->db->query($data)->row()->jns_ang;
+}   
+
+function jns_ang_bok($skpd)
+{
+    //  echo 'DIKA';
+    $ci =& get_instance();
+    $data = "SELECT jns_ang FROM bok_trhrka a WHERE a.kd_skpd='$skpd' AND a.tgl_dpa IN(SELECT MAX(tgl_dpa) FROM bok_trhrka c WHERE c.kd_skpd=a.kd_skpd AND status='1')";
+    return $ci->db->query($data)->row()->jns_ang;
+}   
+?>
