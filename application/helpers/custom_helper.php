1dbab58ec3772000065d5acdbe222455c2856ff4
Emon Krismon <krismonsetiawan57@gmail.com>
2024-08-06 15:18:06 +0700

    fix : sp3b bok

--- application/helpers/custom_helper.php
+++ application/helpers/custom_helper.php
@@ -14,5 +14,11 @@ function jns_ang_bok($skpd)
     $ci =& get_instance();
     $data = "SELECT jns_ang FROM bok_trhrka a WHERE a.kd_skpd='$skpd' AND a.tgl_dpa IN(SELECT MAX(tgl_dpa) FROM bok_trhrka c WHERE c.kd_skpd=a.kd_skpd AND status='1')";
     return $ci->db->query($data)->row()->jns_ang;
-}   
+}
+
+function getBulan($tanggal) {
+    $pecah = explode('-', $tanggal);
+    return $pecah[1];
+}
+
 ?>
